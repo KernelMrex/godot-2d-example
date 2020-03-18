6f1ae058f1fe3ebb4fae70b386fb17098a5cf227
user <user@localhost.localdomain>
2020-03-18 23:50:16 +0300

    First commit

---
+++ Mob.gd
@@ -0,0 +1,18 @@
+extends Node
+
+export var min_speed = 150
+export var max_speed = 250
+
+var mob_types = ["walk", "swim", "fly"]
+
+
+func _ready():
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+
+func _process(delta):
+	pass
+
+
+func _on_Visibility_screen_exited():
+	queue_free()
